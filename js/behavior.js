a8f33b380696df02d6b899c78d8d48895bf1018c
Clifford Denis <dencliff1990@gmail.com>
2020-08-11 22:18:45 -0400

    major changes: added mobile overlay menu and js for links

--- js/behavior.js
+++ js/behavior.js
@@ -12,17 +12,43 @@ function getMainfromSelectorId(selectorId) {
 }
 
 function showArticle(currentSelectorId) {
-    // display element of currentId
+    var mobile = false;
+
+    // Figure out where we're being called from 
+    String (currentSelectorId);
+    if (currentSelectorId.substring(0, 6) == "mobile") {
+        mobile = true;
+        currentSelectorId = currentSelectorId.substring(6, currentSelectorId.length);
+    }
+
+    // Display element of currentId
     var currentMainId = getMainfromSelectorId(currentSelectorId);
     var el = document.getElementById(currentMainId);
     el.style.display = "block";
 
-    // hide everything else
+    // Hide everything else
     for (let i = 0; i < mainArticlesList.length; i++) {
         if (mainArticlesList[i] != currentMainId) {
             el = document.getElementById(mainArticlesList[i]);
             el.style.display = "none";
         }  
-    }    
+    } 
+    
+    // Close mobile menu
+    setTimeout(function() {
+        if (mobile) {
+            closeMobileMenu();
+        }
+    }, 500);
+
 }
 
+function openMobileMenu() {
+    // Make the mobile menu take up all the screen
+    document.getElementById("mobileOverlay").style.width = "100%";
+}
+
+function closeMobileMenu() {
+    // Move the mobile menu back out of view
+    document.getElementById("mobileOverlay").style.width = "0%";
+}
